961a86bab8eb8bb30bc5f8eee114ea3d1fe09578
niwla23 <alwin@cloudserver.click>
2022-05-04 20:13:08 +0200

    show macs

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/niwla23/netwatch
 go 1.18
 
 require (
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-ping/ping v1.0.0 // indirect
+	github.com/j-keck/arping v1.0.2 // indirect
+	github.com/korylprince/ipnetgen v1.0.1 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
--- main.go
+++ main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
 	"sync"
 	"time"
 
+	"github.com/fatih/color"
 	"github.com/go-ping/ping"
+	"github.com/j-keck/arping"
+	"github.com/korylprince/ipnetgen"
 )
 
 type HostAnalysis struct {
 	ping_stats ping.Statistics
 	hostnames  []string
+	hw_adress  *net.HardwareAddr
+	up         bool
 }
 
-func analyze_host(address net.IPAddr, stats chan HostAnalysis, wg *sync.WaitGroup) {
+func analyze_host(address net.IPAddr, timeout int, stats chan HostAnalysis, wg *sync.WaitGroup) {
 	defer wg.Done()
 	pinger := ping.New("")
 	pinger.SetIPAddr(&address)
 
 	pinger.Count = 1
-	pinger.Timeout = 5 * time.Second
+	pinger.Timeout = time.Duration(timeout) * time.Millisecond
 
 	err := pinger.Run()
 	if err != nil {
@@ -28,26 +35,61 @@ func analyze_host(address net.IPAddr, stats chan HostAnalysis, wg *sync.WaitGrou
 	}
 
 	hostnames, _ := net.LookupAddr(address.IP.String())
+	analysis := HostAnalysis{ping_stats: *pinger.Statistics(), hostnames: hostnames, up: true}
 
-	analysis := HostAnalysis{ping_stats: *pinger.Statistics(), hostnames: hostnames}
+	if analysis.ping_stats.PacketLoss != 100 {
+		if hwAddr, _, err := arping.Ping(address.IP); err == nil {
+			analysis.hw_adress = &hwAddr
+		}
+	} else {
+		analysis.up = false
+	}
+
 	stats <- analysis
 }
 
 func main() {
+	var subnet string
+	var timeout int
+	flag.StringVar(&subnet, "subnet", "REQUIRED", "The subnet to scan, e.g. 192.168.178.0/24")
+	flag.IntVar(&timeout, "timeout", 5000, "Timeout for ICMP pings")
+	flag.Parse()
+
+	if subnet == "REQUIRED" {
+		color.Red("The --subnet argument is required")
+		os.Exit(1)
+	}
+
 	wg := new(sync.WaitGroup)
 
 	stats := make(chan HostAnalysis)
 
-	for i := 0; i < 255; i++ {
-		ipv4 := net.IPv4(192, 168, 178, byte(i))
-		wg.Add(1)
-		go analyze_host(net.IPAddr{IP: ipv4, Zone: ""}, stats, wg)
+	gen, err := ipnetgen.New(subnet)
+	if err != nil {
+		color.Red(err.Error())
+		os.Exit(1)
 	}
 
-	for i := 0; i < 100; i++ {
+	for ip := gen.Next(); ip != nil; ip = gen.Next() {
+		wg.Add(1)
+		go analyze_host(net.IPAddr{IP: ip, Zone: ""}, timeout, stats, wg)
+	}
+
+	ip_color := color.New(color.FgBlue)
+	hostname_color := color.New(color.FgGreen)
+	mac_color := color.New(color.FgYellow)
+
+	for i := 0; i < 255; i++ {
 		stats_received := <-stats
-		if stats_received.ping_stats.PacketLoss != 100 {
-			fmt.Println(stats_received.ping_stats.Addr, stats_received.hostnames)
+		if stats_received.up {
+			ip_color.Print(stats_received.ping_stats.Addr)
+			fmt.Print(" ")
+			hostname_color.Print(stats_received.hostnames)
+			if stats_received.hw_adress != nil {
+				fmt.Print(" ")
+				mac_color.Print(stats_received.hw_adress)
+			}
+			fmt.Println()
 		}
 	}
 
